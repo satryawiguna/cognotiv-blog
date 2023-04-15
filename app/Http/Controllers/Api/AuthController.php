617cee32654e6d1644e2b110559b76f090195ecb
satryawiguna <satrya@freshcms.net>
2023-04-15 23:48:00 +0800

    CNV-0004: Add login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,7 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 
+use App\Http\Requests\User\LoginRequest;
+use App\Services\Contracts\IUserService;
+
 class AuthController extends ApiBaseController
 {
+    private readonly IUserService $_userService;
 
+    /**
+     * @param IUserService $userService
+     */
+    public function __construct(IUserService $userService)
+    {
+        $this->_userService = $userService;
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $loginResponse = $this->_userService->login($request);
+
+        if ($loginResponse->isError()) {
+            return $this->getErrorLatestJsonResponse($loginResponse);
+        }
+
+        return $this->getObjectJsonResponse($loginResponse);
+    }
 }
